c938097c183ad1a5436e4b3de61e404445712153
cosmo <cosmo@dhcp-v210-211.vp.reshsg.uci.edu>
2015-07-17 23:04:59 -0700

    Initial Commit

---
+++ EasyIdea/PersonalViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EasyIdea
+//
+//  Created by cosmo on 7/17/15.
+//  Copyright (c) 2015 cosmo. All rights reserved.
+//
+
+import UIKit
+
+class PersonalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
